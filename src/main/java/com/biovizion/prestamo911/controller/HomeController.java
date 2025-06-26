0fac90e3f3fd0ed0b9df7dd5f3195bce781691aa
bryan-reactdev <bryan.reactdev@gmail.com>
2025-06-26 17:18:57 -0600

    Main Page y Dashboard

--- src/main/java/com/biovizion/prestamo911/controller/HomeController.java
+++ src/main/java/com/biovizion/prestamo911/controller/HomeController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("")
 public class HomeController {
-
-
     @GetMapping("/home")
     public String mostrarIndex() {
         return "/home/home";
     }
-
-    @GetMapping("/")
+    @GetMapping("/select")
     public String SelectoOption() {
         return "/home/landingPage";
     }
-
+    
+    @GetMapping("/admin/dashboard")
+    public String mostrarAdminDashboard() {
+        return "/appDashboard/admin/index";
+    }
+    @GetMapping("/usuarioTemp/dashboard")
+    public String mostrarUsuarioDashboard() {
+        return "/appDashboard/admin/index";
+    }
 }
