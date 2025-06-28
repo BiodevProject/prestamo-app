e6148be99cf879d7e206275b4f1cbb535b86ec86
Edwin NM <133041886+EdwinNM22@users.noreply.github.com>
2025-06-27 20:13:48 -0600

    mejoras para el boton de whatsapp

---
+++ src/main/java/com/biovizion/prestamo911/GlobalAttributes.java
@@ -0,0 +1,30 @@
+package com.biovizion.prestamo911;
+
+import com.biovizion.prestamo911.entities.UsuarioEntity;
+import com.biovizion.prestamo911.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.security.Principal;
+
+@Component
+@ControllerAdvice
+public class GlobalAttributes {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @ModelAttribute
+    public void addUserData(Model model, Principal principal) {
+        if (principal != null) {
+            UsuarioEntity usuario = usuarioService.findByEmail(principal.getName())
+                    .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+            model.addAttribute("usuario", usuario);
+
+        }
+    }
+}
